00ac3eaab319b9bbb7c7fd2f4f7d8666e18fedb0
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-08-18 23:32:23 +0200

    Set conditional element rendering

--- app/script.js
+++ app/script.js
@@ -16,15 +16,44 @@ class App extends React.Component {
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
+
+        {this.state.status === 'off'
+        ?
+        <div>
+            <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+            <p>This app will help you track your time and inform you when it's time to rest.</p>
+        </div>
+        : ''
+        }
+        
+        {this.state.status === 'work'
+        ? <img src="./images/work.png" />
+        : ''
+        }
+        
+        {this.state.status === 'rest'
+        ? <img src="./images/rest.png" />
+        : ''
+        }
+        
+        {this.state.status !== 'off'
+        ?
         <div className="timer">
           18:23
         </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
+        : ''
+        }
+        
+        {this.state.status === 'off'
+        ? <button className="btn">Start</button>
+        : ''
+        }
+
+        {this.state.status !== 'off'
+        ? <button className="btn">Stop</button>
+        : ''
+        }
+        
         <button className="btn btn-close">X</button>
       </div>
     )
